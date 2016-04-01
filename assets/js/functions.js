1838b4e5e902df8ac8089b2e9a0d65f1f46f8635
giTomas <tomas.kosegi@gmail.com>
2016-04-01 04:09:47 +0200

    nav

--- assets/js/functions.js
+++ assets/js/functions.js
@@ -1,19 +1,20 @@
 $( document ).ready(function() {
-    
-    
+
+var nav = $('nav')
+
 $(window).scroll(function() {
-    
+
     var wScroll = $(this).scrollTop();
     var wHeight = $(this).height();
-        
-    if (wScroll >= wHeight - 30) {
+
+    if (wScroll >= wHeight) {
             nav.addClass('is-fixed');
         } else {
             nav.removeClass('is-fixed');
     };
-        
+
 }); // end of scroll
-    
+
 $('.menu').on('click', function() {
     $('.navigation').toggleClass('is-showing');
     $('.hamburger').toggleClass('is-hidden');
@@ -22,7 +23,25 @@ $('.menu').on('click', function() {
     if (!nav.hasClass('is-fixed')){
         nav.addClass('is-fixed');
     };
-    
+
 }); //end of menu function
+
+///////////////////
+//smooth scrolling:
+////////////////// //https://css-tricks.com/snippets/jquery/smooth-scrolling/
+
+
+  $('a[href*="#"]:not([href="#"])').click(function() {
+    if (location.pathname.replace(/^\//,'') == this.pathname.replace(/^\//,'') && location.hostname == this.hostname) {
+      var target = $(this.hash);
+      target = target.length ? target : $('[name=' + this.hash.slice(1) +']');
+      if (target.length) {
+        $('html, body').animate({
+          scrollTop: target.offset().top
+        }, 800);
+        return false;
+      }
+    }
+  });
 
 }); //end of dc ready
